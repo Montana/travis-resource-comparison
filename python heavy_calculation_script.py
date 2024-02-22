e3dd393072d4d3bd178c30ab7d07f9c5da3bc159
Michael Mendy <montana@linux.com>
2024-02-22 10:52:16 -0800

    fibonacci calc py script I made in vim

---
+++ python heavy_calculation_script.py
@@ -0,0 +1,25 @@
+def fib_memo(n, memo={}):
+    """
+    Calculate the nth Fibonacci number using memoization to optimize.
+    :param n: The nth Fibonacci number to calculate.
+    :param memo: Dictionary to store previously calculated Fibonacci numbers.
+    :return: The nth Fibonacci number.
+    """
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
+    return memo[n]
+
+def heavy_calculation():
+    """
+    Perform heavy calculations by calculating a large Fibonacci number.
+    """
+    n = 1000  # Change this value for larger or smaller calculations
+    print(f"Calculating the {n}th Fibonacci number...")
+    fib_number = fib_memo(n)
+    print(f"The {n}th Fibonacci number is: {fib_number}")
+
+if __name__ == "__main__":
+    heavy_calculation()
